0666016c51fda69737ad9f6bf4a93f17bea25489
Łukasz <devlukaszgreiner@gmail.com>
2024-03-05 12:03:17 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+document.write("This message was written via JavaScript");
+self.addEventListener("fetch", (event) => {
+  // This is a dummy event listener
+  // just to pass the PWA installation criteria on
+  // some browsers
+});
